ee7e9ebf9d7963cb90b542622f7f63011b588794
Alexandre Grassart <alex.grassart@gmail.com>
2020-02-20 14:44:14 +0100

    Ateliers Terminés

--- src/Entity/CreateAtelier.php
+++ src/Entity/CreateAtelier.php
@@ -45,12 +45,12 @@ class CreateAtelier
         return $this->id;
     }
 
-    public function getImage(): ?string
+    public function getImage()
     {
         return $this->image;
     }
 
-    public function setImage(?string $image): self
+    public function setImage($image): self
     {
         $this->image = $image;
 
